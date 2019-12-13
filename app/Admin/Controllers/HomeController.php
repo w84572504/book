e68dc988c86b591e81a615d6956cdc71ff1d49ed
wang <123@qq.com>
2019-12-13 15:37:10 +0800

    first commit

---
+++ app/Admin/Controllers/HomeController.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Admin\Controllers;
+
+use App\Http\Controllers\Controller;
+use Encore\Admin\Controllers\Dashboard;
+use Encore\Admin\Layout\Column;
+use Encore\Admin\Layout\Content;
+use Encore\Admin\Layout\Row;
+
+class HomeController extends Controller
+{
+    public function index(Content $content)
+    {
+        return $content
+            ->title('首页')
+            ->description('首页描述')
+            ->row(Dashboard::title())
+            ->row(function (Row $row) {
+
+                $row->column(4, function (Column $column) {
+                    $column->append(Dashboard::environment());
+                });
+
+                $row->column(4, function (Column $column) {
+                    $column->append(Dashboard::extensions());
+                });
+
+                $row->column(4, function (Column $column) {
+                    $column->append(Dashboard::dependencies());
+                });
+            });
+    }
+    public function test(Content $content)
+    {
+        return $content
+            ->title('测试')
+            ->description('测试描述');
+    }
+}
